9961a85c8d8e3fec896db4bbcc46de6930461523
umut karaytu <128383118+Karaytumut20@users.noreply.github.com>
2024-02-20 02:47:43 +0300

    update

--- src/pages/category/details/[id].tsx
+++ src/pages/category/details/[id].tsx
@@ -3,10 +3,10 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 
 const testData = {
-  website: "www.test.com",
-  phone: "05462476452",
-  degree: "Birleşmiş milletler başkanı",
-  name: "umut karaytu",
+  website: "Test Title",
+  phone: "65462476452",
+  degree: "Test Degree",
+  name: "Test name",
 };
 
 const DetailsCategory = (props: { id: any }) => {
@@ -33,78 +33,11 @@ const DetailsCategory = (props: { id: any }) => {
     <div style={{ position: "relative", textAlign: "center" }}>
       {data.image && (
         <div style={{ position: "relative", display: "inline-block" }}>
-          <img
-            src={data.image}
-            alt="Afiş"
-            height={850}
-            width={650}
-            style={{ display: "block", margin: "0 auto" }}
-          />
-          <div
-            style={{
-              position: "absolute",
-              bottom: 0,
-              left: 0,
-              width: "100%",
-              zIndex: 100,
-              padding: "10px",
-            }}
-          >
-            <div
-              style={{
-                display: "flex",
-                justifyContent: "space-between",
-                alignItems: "center",
-              }}
-            >
-
-              <div style={{ textAlign: "left" }}>
-                <p style={{ margin: "0", color: "white" }}>
-                  {testData.website}
-                </p>
-                <p style={{ margin: "0", color: "white" }}>
-                  {testData.phone}
-                </p>
-                
-              </div>
-              
-              
-
-             
-              <svg
-                width="50%"
-                height="2"
-                style={{ flex: "1", minWidth: "50px", marginLeft: "10px" }}
-              >
-                <line
-                  x1="0"
-                  y1="1"
-                  x2="100%"
-                  y2="1"
-                  stroke="white"
-                  strokeWidth="2"
-                />
-              </svg>
-              <div style={{ color: "white", textAlign: "right" }}>
-                <p style={{ margin: "0" }}>{testData.degree}</p>
-                <p style={{ margin: "0" }}>{testData.name}</p>
-              </div>
-              
-              <svg
-                width="10%"
-                height="2"
-                style={{ flex: "1", minWidth: "50px", marginLeft: "10px" }}
-              >
-                <line
-                  x1="0"
-                  y1="1"
-                  x2="10%"
-                  y2="1"
-                  stroke="white"
-                  strokeWidth="2"
-                />
-              </svg>
-            </div>
+          <img src={data.image} alt="Afiş" height={850} width={650} style={{ display: "block", margin: "0 auto" }} />
+          <div style={{ position: "absolute", bottom: 0, left: 0, zIndex: 100, padding: "10px", display: "flex", alignItems: "center" }}>
+            {data.logo && (
+              <img src={data.logo} alt="Logo" height={120} width={120} />
+            )}      
           </div>
         </div>
       )}
